0ea0c44f6e6e487ec0f60b943cf72ed87e026643
Ahmed Tawfik <teffa.ahmed.1@gmail.com>
2026-08-24 14:28:25 +0300

    Refactor type annotations for consistency in migration script

--- src/db/migration/versions/61a875b563fe_remove_images_column_from_products.py
+++ src/db/migration/versions/61a875b563fe_remove_images_column_from_products.py
@@ -5,16 +5,15 @@ Revises: 23ce02c69b3f
 Create Date: 2026-08-19 13:59:09.324244
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 revision: str = "61a875b563fe"
-down_revision: Union[str, None] = "23ce02c69b3f"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = "23ce02c69b3f"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
