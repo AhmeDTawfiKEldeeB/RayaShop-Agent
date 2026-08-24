aa656100fa9ce36e67130fa3459fc9d9c06b6de7
Ahmed Tawfik <teffa.ahmed.1@gmail.com>
2026-08-24 14:28:19 +0300

    Refactor import statements and type annotations for consistency and clarity

--- src/db/migration/versions/23ce02c69b3f_create_products_table.py
+++ src/db/migration/versions/23ce02c69b3f_create_products_table.py
@@ -4,16 +4,16 @@ Revision ID: 23ce02c69b3f
 Revises:
 Create Date: 2026-08-18 16:52:39.703436
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 revision: str = "23ce02c69b3f"
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
